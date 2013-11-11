699dcf8fb5a1d9b48c4c328ade16f09ac4e5fc7c
Gaston Sanchez <gaston.stat@gmail.com>
2013-11-11 12:57:01 +0100

    add is_factor_dataframe

--- R/is-dataframe.r
+++ R/is-dataframe.r
@@ -4,14 +4,15 @@
 #' \code{is_dataframe} tests if an object is a data frame \cr
 #' \code{is_numeric_dataframe} tests if an object is a numeric data frame \cr
 #' \code{is_string_dataframe} tests if an object is a string data frame \cr
+#' \code{is_factor_dataframe} tests if an object is a factor data frame \cr
 #' \code{is_not_dataframe} tests if an object is not a data frame 
 #' 
 #' @param x an R object
 #' @name is_dataframe
 #' @aliases is_dataframe is_numeric_dataframe is_string_dataframe
-#' is_not_dataframe
+#' is_factor_dataframe is_not_dataframe
 #' @export is_dataframe is_numeric_dataframe is_string_dataframe
-#' is_not_dataframe
+#' is_factor_dataframe is_not_dataframe
 #' @examples
 #' is_dataframe(iris) # TRUE
 #' is_dataframe(1:10) # FALSE
@@ -22,8 +23,12 @@
 #' DF = matrix(letters[1:24], 6, 4)
 #' DF1 = data.frame(DF)
 #' DF2 = data.frame(DF, stringsAsFactors=FALSE)
+#' 
 #' is_string_dataframe(DF1) # FALSE
 #' is_string_dataframe(DF2) # TRUE
+#' 
+#' is_factor_dataframe(DF1) # TRUE
+#' is_factor_dataframe(DF2) # FALSE
 NULL
 
 is_dataframe <- function(x) {
@@ -41,6 +46,13 @@ is_string_dataframe <- function(x) {
   if (is.data.frame(x)) {
     characters = unlist(lapply(x, is.character))
     if (sum(characters) == dim(x)[2L]) TRUE else FALSE
+  } else FALSE
+}
+
+is_factor_dataframe <- function(x) {
+  if (is.data.frame(x)) {
+    factors = unlist(lapply(x, is.factor))
+    if (sum(factors) == dim(x)[2L]) TRUE else FALSE
   } else FALSE
 }
 
