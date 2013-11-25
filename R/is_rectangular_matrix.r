a5f26e381c3744406e44229c0d46abcc1c2ffffc
Gaston Sanchez <gaston.stat@gmail.com>
2013-11-25 21:36:05 +0100

    add tall and wide

--- R/is_rectangular_matrix.r
+++ R/is_rectangular_matrix.r
@@ -1,19 +1,36 @@
 #' @title Is rectangular matrix
 #' 
-#' @description Test if an object is a rectangular matrix (or not)
-#' (i.e. number of rows different from number of columns)
+#' @description 
+#' \code{is_rectangular_matrix(x)} tests whether \code{x} is a rectangular 
+#' matrix (i.e. number of rows different from number of columns) \cr
+#' \code{is_tall_matrix(x)} tests whether \code{x} is a matrix with more
+#' rows than columns \cr
+#' \code{is_wide_matrix(x)} tests whether \code{x} is a matrix with more
+#' columns than rows
 #' 
 #' @param x an R object
 #' @seealso \code{\link{is_matrix}}, \code{\link{is_square_matrix}}
 #' @aliases is_rectangular_matrix is_not_rectangular_matrix
+#' is_tall_matrix is_wide_matrix
 #' @export is_rectangular_matrix is_not_rectangular_matrix
+#' is_tall_matrix is_wide_matrix
 #' @examples
-#' m1 = matrix(1:12, 4, 3)
-#' m2 = matrix(1:9, 3, 3)
+#' rec = matrix(1:12, 4, 3)
+#' tall = matrix(1:21, 7, 3)
+#' wide = matrix(1:21, 3, 7)
+#' sqr = matrix(1:9, 3, 3)
 #' 
-#' is_rectangular_matrix(m1) # TRUE
-#' is_rectangular_matrix(m2) # FALSE
-#' is_not_rectangular_matrix(m2) # TRUE
+#' is_rectangular_matrix(rec)  # TRUE
+#' is_rectangular_matrix(sqr)  # FALSE
+#' is_not_rectangular_matrix(sqr)  # TRUE
+#' 
+#' is_tall_matrix(tall)  # TRUE
+#' is_tall_matrix(wide)  # FALSE
+#' is_tall_matrix(sqr)  # FALSE
+#' 
+#' is_wide_matrix(wide)  # TRUE
+#' is_wide_matrix(tall)  # FALSE
+#' is_wide_matrix(sqr)  # FALSE
 is_rectangular_matrix <- function(x) {
   if (is.matrix(x)) {
     if (nrow(x) != ncol(x)) TRUE else FALSE      
@@ -24,4 +41,16 @@ is_not_rectangular_matrix <- function(x) {
   if (is.matrix(x)) {
     if (nrow(x) == ncol(x)) TRUE else FALSE      
   } else TRUE
+}
+
+is_tall_matrix <- function(x) {
+  if (is.matrix(x)) {
+    if (nrow(x) > ncol(x)) TRUE else FALSE
+  } else FALSE
+}
+
+is_wide_matrix <- function(x) {
+  if (is.matrix(x)) {
+    if (nrow(x) < ncol(x)) TRUE else FALSE
+  } else FALSE
 }
